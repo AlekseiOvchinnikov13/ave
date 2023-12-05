13aeb77026c0b3a0346d22867fb7a999037f3e66
Aleksei Ovchinnikov <eastonsc6@yandex.ru>
2023-12-05 23:34:36 +0300

    add screen 10

--- src/app/components/AILanding/Screens/Screen10/index.js
+++ src/app/components/AILanding/Screens/Screen10/index.js
@@ -1,11 +1,23 @@
+'use client';
 import styles from './Screen10.module.scss';
 import SectionContainer from '../../SectionContainer';
 import ContentContainer from '../../ContentContainer';
 import Background from '../../Background';
 import BgScreen10 from '../../../../../../public/images/backgrounds/bg-screen10.jpg';
 import PageTitle from '../../PageTitle';
+import Paragraph from '../../Paragraph';
+import {speakers} from '../../../../../data/speakers';
+import Image from 'next/image';
+import {AnimatePresence, motion} from 'framer-motion';
+import uuid from 'react-uuid';
+import {colorWhite} from '../../../../styles/variables.module.scss';
+import {useState} from 'react';
 
 const Screen10 = () => {
+  const [activeSpeaker, setActiveSpeaker] = useState(speakers[0]);
+  const onClickHandler = name => {
+    setActiveSpeaker(speakers.find(speaker => speaker.name.label === name));
+  };
 
   return (
     <SectionContainer>
@@ -15,7 +27,37 @@ const Screen10 = () => {
       />
       <ContentContainer className={styles.container}>
         <PageTitle text={'спикеры'} className={styles.title}/>
-        
+        <div className={styles.speakers}>
+          <Paragraph> Кликни, чтобы узнать больше про остальных</Paragraph>
+          <div className={styles.speakerImageWrapper}>
+            {speakers.map(speaker =>
+              <motion.button
+                className={styles.speakerImage}
+                key={uuid()}
+                initial={{borderColor: activeSpeaker?.name?.label === speaker.name.label ? speaker.name.color : colorWhite}}
+                whileHover={{scale: 1.1}}
+                transition={{ease: 'easeInOut', duration: .3}}
+                onClick={() => onClickHandler(speaker.name.label)}
+              >
+                <Image
+                  src={speaker.srcSmall}
+                  alt={speaker.name.label}
+                  fill
+                />
+              </motion.button>
+            )}
+          </div>
+          <div className={styles.info}>
+            <h5 style={{color: activeSpeaker?.name?.color}}>
+              {activeSpeaker?.name?.label}
+            </h5>
+            <div className={styles.description}>
+              {activeSpeaker?.description?.map(p =>
+                <Paragraph>{p}</Paragraph>
+              )}
+            </div>
+          </div>
+        </div>
       </ContentContainer>
     </SectionContainer>
   );
